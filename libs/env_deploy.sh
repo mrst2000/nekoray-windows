f4e908e15ecad3f0298ab9a33baf1b44e6ca3ed3
mrst2000 <75087475+mrst2000@users.noreply.github.com>
2024-04-28 20:45:52 +0000

    Update env_deploy.sh

--- libs/env_deploy.sh
+++ libs/env_deploy.sh
@@ -1,4 +1,4 @@
 SRC_ROOT="$PWD"
 DEPLOYMENT="$SRC_ROOT/deployment"
 BUILD="$SRC_ROOT/build"
-version_standalone="nekorayx-$(curl -s https://raw.githubusercontent.com/mrst2000/nekoray/main/nekoray_version.txt | sed 's/[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}//' | rev | sed 's/-//' | rev)"
+version_standalone="MrLinux-$(curl -s https://raw.githubusercontent.com/mrst2000/nekoray/main/nekoray_version.txt | sed 's/[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}//' | rev | sed 's/-//' | rev)"
